43159351cdc46f127a66bd333895007e86c2fcb8
Baylor Rae <baylor@thecodedeli.com>
2017-07-03 20:14:38 -0500

    add gem: faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PROJECT_TITLES = 10.times.map { Faker::Internet.domain_word }
