495745f9b05e48544ebcf44f77c8df160b99b0c0
Baylor Rae <baylor@thecodedeli.com>
2017-07-03 20:18:02 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 PROJECT_TITLES = 10.times.map { Faker::Internet.domain_word }
+
+SPLIT = 0.4
+OFFSET = PROJECT_TITLES.length * SPLIT
+
+PROJECT_USERS = {
+  'leo@example.com' => PROJECT_TITLES[0, OFFSET],
+  'bob@example.com' => PROJECT_TITLES[OFFSET..-1]
+}
+
+PROJECT_USERS.each do |email, projects|
+  user = User.new(email: email, password: 'secret')
+  projects.each { |title| user.projects.build(title: title) }
+  user.save
+end
